8897535855fd673cf5e32978041f22d8bfd5bdf2
Francisco Bolívar <francisco.bolivar@nazaries.com>
2023-12-13 12:22:03 +0100

    Bump to v0.1.0 version

--- lib/decidim/centers/version.rb
+++ lib/decidim/centers/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-centers version.
   module Centers
-    VERSION = "0.1.0.dev"
+    VERSION = "0.1.0"
     DECIDIM_VERSION = "0.27.4"
     COMPAT_DECIDIM_VERSION = [">= 0.27.0", "< 0.28"].freeze
   end
